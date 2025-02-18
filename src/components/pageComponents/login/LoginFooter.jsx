8e03c03608c264d19364c0be5ef2e9df03b7d8ca
Dilan kanishka <dilankanishka032@gmail.com>
2025-02-18 10:22:39 +0530

    update text

--- src/components/pageComponents/login/LoginFooter.jsx
+++ src/components/pageComponents/login/LoginFooter.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function LoginFooter() {
   return (
     <div className='absolute bottom-5 md:text-right text-center w-full'>
-        <p className='text-gray-600'>Develop By Nanosoft solution PVT(Ltd)</p>
+        <p className='text-gray-600'>Developed by Nanosoft solution PVT(Ltd)</p>
     </div>
   )
 }
